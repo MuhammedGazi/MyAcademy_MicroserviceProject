7aa365132af5586cb486ea48f4168615c789de66
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2026-03-06 21:58:10 +0300

    ıdentity server added

---
+++ IdentityServer/ECommerce.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.IdentityServer.DTOs;
+using ECommerce.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                LastName = model.LastName,
+                FirstName = model.FirstName,
+            };
+            var result=await _userManager.CreateAsync(user,model.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok("kullanıcı kaydı başarılı");
+        } 
+    }
+}
